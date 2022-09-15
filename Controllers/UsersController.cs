bf074e52701d640e557e02be025457ff344e41ad
Felipe Bueno <lipe_que@hotmail.com.br>
2022-09-14 20:23:09 -0400

    Adição de métodos e padronização do código.
    Adição de métodos ao controlador de usuário e atualização dos componentes do projeto para o idioma inglês.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaMissaCore.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace MinhaMissaCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> Users = new List<User>
+        {
+                new User
+                {
+                    Id = 1,
+                    Name = "Felipe Queiroz",
+                    Email = "felipequeiroz@minhamissa.com",
+                    Password = "12345",
+                    Role = "Administrador"
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "Gabriel Laroca",
+                    Email = "gabriellaroca@minhamissa.com",
+                    Password = "12345",
+                    Role = "Administrador"
+                },
+                new User
+                {
+                    Id = 3,
+                    Name = "Walisson Ferraz",
+                    Email = "walissonferraz@minhamissa.com",
+                    Password = "12345",
+                    Role = "Administrador"
+                }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            return Ok(Users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<User>>> GetById(int id)
+        {
+            var user = Users.Find(u => u.Id == id);
+
+            if (user is null)
+            {
+                return BadRequest("User not found");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            Users.Add(user);
+            return Ok(Users);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User userRequest)
+        {
+            var userToUpdate = Users.Find(u => u.Id == userRequest.Id);
+
+            if (userToUpdate is null)
+            {
+                return BadRequest("User not found");
+            }
+
+            userToUpdate.Name = userRequest.Name;
+            userToUpdate.Email = userRequest.Email;
+            userToUpdate.Password = userRequest.Password;
+            userToUpdate.Role = userRequest.Role;
+
+            return Ok(Users);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> Delete(int id)
+        {
+            var userToDelete = Users.Find(u => u.Id == id);
+
+            if (userToDelete is null)
+            {
+                return BadRequest("User not found");
+            }
+
+            Users.Remove(userToDelete);
+            return Ok(userToDelete);
+        }
+    }
+}
---
+++ Model/Event.cs
@@ -0,0 +1,39 @@
+﻿namespace MinhaMissaCore.Model
+{
+    public class Event
+    {
+        /// <summary>
+        /// Identificação do evento.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nome do Evento, título.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// Descrição do evento. Conta resumidamente como será o evento.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+        /// <summary>
+        /// Dia de início do evento.
+        /// </summary>
+        public DateTime StartDate { get; set; } = default!;
+        /// <summary>
+        /// Data de encerramento do evento.
+        /// </summary>
+        public DateTime LastDate { get; set; } = default!;
+        /// <summary>
+        /// Local do evento.
+        /// </summary>
+        public string Local { get; set; } = string.Empty;
+        /// <summary>
+        /// Acentos no evento (lugares).
+        /// </summary>
+        public int Places { get; set; } = default!;
+        /// <summary>
+        /// Indica se o usuário está ativo.
+        /// </summary>
+        public bool Active { get; set; } = default!;
+        
+    }
+}

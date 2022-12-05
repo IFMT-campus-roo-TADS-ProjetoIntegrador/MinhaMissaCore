bfd0afc0235e973f361a635e5e6285c6a5211e33
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2022-12-05 18:00:24 -0300

    Criação dos controllers Institution e Events

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -34,5 +34,52 @@ namespace MinhaMissaCore.Controllers
 
             return Ok(eventToGet);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Event>>> AddEvent(Event newEvent)
+        {
+            await this.dataContext.AddAsync(newEvent);
+            await this.dataContext.SaveChangesAsync();
+
+            return Ok(newEvent);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Event>>> UpdateEvent (Event eventRequest)
+        {
+            var eventToUpdate = await this.dataContext.Events.FindAsync(eventRequest);
+
+            if (eventToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            eventToUpdate.Name = eventRequest.Name;
+            eventToUpdate.Location = eventRequest.Location;
+            eventToUpdate.Description = eventRequest.Description;
+            eventToUpdate.StartDate = eventRequest.StartDate;
+            eventToUpdate.Duration = eventRequest.Duration;
+            eventToUpdate.Seats = eventRequest.Seats;
+            eventToUpdate.Instituition = eventRequest.Instituition;
+
+
+            await this.dataContext.SaveChangesAsync();
+            return Ok(Events);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Event>>> Delete(int id)
+        {
+            var eventToDelete = await this.dataContext.Events.FindAsync(id);
+
+            if (eventToDelete == null)
+            {
+                return NotFound();
+            }
+
+            this.dataContext.Events.Remove(eventToDelete);
+            await this.dataContext.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
---
+++ Controllers/InstituitionController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using MinhaMissaCore.Model;
+
+namespace MinhaMissaCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InstituitionController : ControllerBase
+    {
+        private static List<Instituition> Instituitions = new List<Instituition>();
+
+        public DataContext dataContext;
+        public InstituitionController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Instituition>>> Get()
+        {
+            return Ok(Instituitions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Instituition>>> GetById(int id)
+        {
+            var instituitions = await this.dataContext.Intituitions.FindAsync(id);
+
+            if (instituitions is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Instituitions);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Instituition>>> AddInstituition(Instituition instituition)
+        {
+            await this.dataContext.AddAsync(instituition);
+            await this.dataContext.SaveChangesAsync();
+
+            return Ok(Instituitions);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Instituition>>> UpdateInstituition(Instituition instituition)
+        {
+            var instituitionToUpdate = await this.dataContext.Intituitions.FindAsync(instituition);
+
+            if (instituitionToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            instituitionToUpdate.Name = instituition.Name;
+            instituitionToUpdate.Address = instituition.Address;
+
+            await this.dataContext.SaveChangesAsync();
+            return Ok(Instituitions);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Instituition>>> Delete(int id)
+        {
+            var instituitionToDelete = await this.dataContext.Intituitions.FindAsync(id);
+
+            if (instituitionToDelete == null)
+            {
+                return NotFound();
+            }
+
+            this.dataContext.Intituitions.Remove(instituitionToDelete);
+            await this.dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}

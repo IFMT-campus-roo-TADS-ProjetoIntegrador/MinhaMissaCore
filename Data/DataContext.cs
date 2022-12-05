d72ce5d02671dfac3e5bfac54f0a879fb6937451
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2022-12-05 18:00:33 -0300

    Update DataContext.cs

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace MinhaMissaCore.Data
         }
         public DbSet<Event> Events { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Instituition> Intituitions { get; set; }
     }
 }
